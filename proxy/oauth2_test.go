aba7a24285545e79903975b1c7790a2ca5e1db5c
Paride Martino <paride5745@users.noreply.github.com>
2023-01-27 13:48:56 +0100

    using the correct function

--- proxy/oauth2_test.go
+++ proxy/oauth2_test.go
@@ -216,7 +216,7 @@ func TestRouteRecoverOriginRedirectURI(t *testing.T) {
 			g.Expect(test.expectHTTPCode).To(Equal(w.Code))
 
 			for k, v := range test.expectHeaders {
-				g.Expect(v).To(Equal(test.request().Response.Header[k]))
+				g.Expect(v).To(Equal(w.Result().Request.Header[k]))
 			}
 		})
 	}
@@ -386,7 +386,7 @@ func TestChangeRedirectURI(t *testing.T) {
 			g.Expect(test.expectHTTPCode).To(Equal(w.Code))
 
 			for k, v := range test.expectHeaders {
-				g.Expect(v).To(Equal(test.request().Response.Header[k]))
+				g.Expect(v).To(Equal(w.Result().Request.Header[k]))
 			}
 
 			if test.expectBody != "" {
